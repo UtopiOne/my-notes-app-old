eb35d54b04bac3c7fd0c85e3522291842ea14d13
UtopiOne <arseniyver@gmail.com>
2023-03-12 14:22:37 +0300

    Changed transition on addCard

--- src/lib/AddCard.svelte
+++ src/lib/AddCard.svelte
@@ -1,12 +1,12 @@
 <script>
 	import plus from '$lib/assets/plus.png';
-	import { blur } from 'svelte/transition';
+	import { fly } from 'svelte/transition';
 </script>
 
 <button
 	on:click
 	class="flex justify-center h-[300px] w-[200px] rounded-lg border-black border-dotted border-2 hover:scale-105 transition ease-in-out"
-	in:blur={{ amount: 10, duration: 100 }}
+	in:fly={{ y: 100, duration: 100 }}
 >
 	<img src={plus} alt="plus sign (press to add note)" class="h-12 w-12 self-center opacity-80" />
 </button>
