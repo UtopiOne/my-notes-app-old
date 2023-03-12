3a900cb4020d868e318086e6438b6c143952fd73
UtopiOne <arseniyver@gmail.com>
2023-03-12 12:22:50 +0300

    Added note adding button

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import AddCard from '$lib/AddCard.svelte';
+
+	let notes;
+</script>
+
+<div class="m-10 grid grid-cols-8 gap-10">
+	<AddCard />
+</div>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,7 @@
 <a href="https://www.flaticon.com/free-icons/note" title="note icons"
 	>Note icons created by Freepik - Flaticon</a
 >
+
+<a href="https://www.flaticon.com/ru/free-icons/" title="плюс иконки"
+	>Plus icon IYAHICON - Flaticon</a
+>
