ba44b1f740735be3a51897077bf87de9a5d25969
UtopiOne <arseniyver@gmail.com>
2023-03-15 21:46:40 +0300

    Added word breaks

--- src/lib/Note.svelte
+++ src/lib/Note.svelte
@@ -10,8 +10,8 @@
 	class="grid grid-cols-[2fr_1fr] grid-rows-[1fr_3fr_1fr] h-[375px] w-full resize-none bg-slate-300 rounded-lg hover:scale-105 transition ease-in-out"
 	in:fly={{ y: 100, duration: 300 }}
 >
-	<button class="col-start-1 col-span-2 text-xl hover:underline">{title}</button>
-	<p class="col-span-2 row-start-2 mx-5">{contents}</p>
+	<button class="col-start-1 col-span-2 text-xl break-all hover:underline">{title}</button>
+	<p class="col-span-2 row-start-2 mx-5 break-all">{contents}</p>
 	<img
 		src={garbage}
 		alt="delete"
