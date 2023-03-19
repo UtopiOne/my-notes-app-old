79376fa72d1d92ee6d007775d9fd7da4cb7f4a80
UtopiOne <arseniyver@gmail.com>
2023-03-19 16:49:19 +0300

    Notes can now be removed

--- src/lib/components/Note.svelte
+++ src/lib/components/Note.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
-	import { fly } from 'svelte/transition';
+	import { fly, scale } from 'svelte/transition';
 	import garbage from '$lib/assets/garbage.png';
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
 
 	export let title: string = '';
 	export let contents: string = '';
@@ -16,14 +19,14 @@
 <div
 	class="grid grid-cols-[2fr_1fr] grid-rows-[1fr_3fr_1fr] h-[375px] w-full resize-none bg-slate-300 rounded-lg hover:scale-105 transition ease-in-out"
 	in:fly={{ y: 100, duration: 300 }}
+	out:fly={{ y: 100, duration: 300 }}
 >
 	<button class="col-start-1 col-span-2 text-xl break-all hover:underline">{title}</button>
 	<p class="col-span-2 row-start-2 mx-5">{cutString(contents)}</p>
-	<img
-		src={garbage}
-		alt="delete"
+	<button
 		class="w-10 h-10 justify-self-center col-start-2 row-start-3 hover:opacity-50"
-	/>
+		on:click={() => dispatch('delete')}><img src={garbage} alt="delete" /></button
+	>
 </div>
 
 <slot />
